e2fad2d21310205dbc2aa867a8d27b020b38a96f
sc22j4s <117829431+sc22j4s@users.noreply.github.com>
2022-12-02 17:30:28 +0000

    asdf

--- leopard.py
+++ leopard.py
@@ -13,7 +13,6 @@ class Leopard:
     def __init__(self, filepath: str) -> None:
         self.filepath = filepath
         
-
         print(filepath)
         # check if file is locatable
         try:
@@ -41,10 +40,11 @@ class Leopard:
             line = file.readline()
             # split line into list and append 
             self.header = line.split(',').copy()
- 
+            # remove '\n' from last element
+            self.header[len(self.header)-1] = self.header[len(self.header)-1].strip()
 
             # check for empty file
-            if(self.header[0] == ""):
+            if(self.header[0] == None):
                 print("empty file.")
                 return
             
@@ -70,7 +70,8 @@ class Leopard:
     def stats(self) -> dict:
         print("\nSTATS\n")
 
-        dict_stats = {}
+        stats = {}
+        dict_list = []
 
         """
             1. make entire stats dict to write smaller dicts to 
@@ -84,7 +85,6 @@ class Leopard:
             3. define static count mean min max dicts within
             4. define sum, count, min, max vars
         """
-        dict_list = []
 
         # iterate for number of columns
 
@@ -96,65 +96,87 @@ class Leopard:
             # must also check if the item is not redundant
 
             # todo: cast as int!!!
-            print(isinstance(int(self.data[0][i]), int))
-            if (not isinstance(self.data[0][i], int)) and self.data[0][i] != 'NA' and self.data[0][i] != '' and self.data[0][i] != '-':
+            if not self.data[0][i].isdigit() and self.data[0][i] != 'NA' and self.data[0][i] != '' and self.data[0][i] != '-':
                 is_valid_column = False
-                print("{0} is not integer. skipping {1}", self.data[0][i], self.header[i])
+                print("{0} is not integer. skipping {1}".format(self.data[0][i], self.header[i]))
 
 
             if is_valid_column:
-                # iterate through column
-                    dict_title = self.header[i]
-                    sum = 0
-                    min = 0
-                    max = 0
-                    count = 0
-                    
-
-                    for k in range(len(self.data)):
-                        item = self.data[k][i]
-                        # skip missing data item
-                        if item == 'NA' and item == '' and item == '-':
-                            is_integer = False
-                            
-                        if is_integer:
-                            count = count + 1
-                            sum = sum + item
-
-                            if item < min:
-                                min = item
-                            if item > max: 
-                                max = item
-                    
-                    # calculate average, rounded 2 d.p
-                    mean = sum / count
-                    mean.round(mean,2)
-
-
-
-
-                    # setup dictionary 
-
-                    dict_title = {
-                        'count': count,
-                        'mean': mean,   
-                        'min': min,
-                        'max': max
-                    }
-                    stats.append(dict_title.copy())
-
-                            
+               # iterate through column
+                dict_title = self.header[i]
+                dict_contents = {}
+                sum = 0
+                # assign min/max variables to data items
+                min = int(self.data[0][i])
+                max = int(self.data[0][i])
+                count = 0
                 
-        
-        return dict_stats
+
+                for k in range(len(self.data)):
+                    is_integer = True
+                    # skip missing data item
+                    if self.data[k][i] == 'NA' or self.data[k][i] == '' or self.data[k][i] == '-':
+                        is_integer = False
+                    
+                    if is_integer:
+                        # cast string as int for processing
+                        item = int(self.data[k][i])
+                        # process variables
+                        count = count + 1
+                        sum = sum + item
+
+                        if item < min:
+                            min = item
+                        if item > max: 
+                            max = item
+                
+                # calculate average, rounded 2 d.p
+                if count > 0:                    
+                    mean = round(sum / count, 2)
+
+                # assign dictionary key/value pairs
+
+                dict_contents = {
+                    'count': count,
+                    'mean': mean,   
+                    'min': min,
+                    'max': max
+                }
+                # print("appending {0} to stats from column {1}".format(dict_contents, dict_title))
+                stats[dict_title] = dict_contents
+
+        return stats
             
 
         # number of entries in header 
         
 
     def html_stats(self, stats: dict, filepath: str) -> None:
-        # delete pass and this comment to write your code
-        pass
+
+
+        with open("filepath.html","w") as html:
+            html.write("<html>\n")
+            html.write("<head>\n")
+            html.write("<meta charset=\"UTF-8\">\n")
+            html.write("<style>\n")
+            html.write("table, th, td {\n")
+            html.write("border: 1px solid black;\n")
+            html.write("border-collapse: collapse;\n")
+            html.write("}\n")
+            html.write("</style>\n")
+            html.write("<title>Student grades</title>\n")
+            html.write("</head>\n")
+            html.write("<body>\n")
+            html.write("<h2>Test scores</h2>\n")
+            html.write("<table>\n")
+
+            for i in stats: 
+                for k in i: 
+                    x=1
+                    
+
+
+
 
     def count_instances(self, criteria) -> int:
         """
@@ -174,31 +196,31 @@ if __name__ == "__main__":
     # test diabetes_data.csv
     test = Leopard("diabetes_data.csv")
     print(test.get_header())
-    print(test.get_data())
+    # print(test.get_data())
     stats = test.stats()
     print(stats)
     test.html_stats(stats, "diabetes.html")
     print()
 
     # test fide2021.csv
-    """
+    
     test2 = Leopard("fide2021.csv")
     print(test2.get_header())
-    print(test.get_data())
+    # print(test.get_data())
     stats2 = test2.stats()
     print(stats2)
     test2.html_stats(stats2, "fide2021.html")
     print()
-    """
+    
     
 
     # test student.csv
-    """
+    
     test3 = Leopard("student.csv")
     print(test3.get_header())
-    print(test.get_data())
+    # print(test.get_data())
     stats3 = test3.stats()
     print(stats3)
     test3.html_stats(stats3, "student.html")
-    """
+    
     
